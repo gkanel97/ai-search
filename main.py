6566564a2397e7fd62fa34afe123cdcea473c7e8
George Kanellopoulos <gkanel97@hotmail.com>
2024-02-27 19:16:57 +0000

    Added time-related experiments

--- main.py
+++ main.py
@@ -24,26 +24,37 @@ if __name__ == '__main__':
     visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/dfs.png')
     path, exploration = maze_solver.a_star()
     visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/a_star.png')
-    value_function, path, history = maze_solver.makrov_value_iteration(iterations=50)
+    value_function, path, history = maze_solver.makrov_value_iteration()
     visualiser.draw_value_policy(history, filename='./figures/value_iteration.gif')
     policy, value_function, path, history = maze_solver.makrov_policy_iteration()
     visualiser.draw_value_policy(history, path, filename='./figures/policy_iteration.gif')
 
-    # import time
 
-    # # Compare the three algorithms for different input sizes
-    # input_sizes = [10, 20, 30, 40, 50, 60]
-    # bfs_times = []
-    # dfs_times = []
-    # a_star_times = []
+    # Compare the three algorithms for different input sizes
+    input_sizes = np.arange(10, 101, 10)
+    random_seeds = np.arange(1, 20)
+    exec_times = {
+        'bfs': np.zeros((len(input_sizes), len(random_seeds))),
+        'dfs': np.zeros((len(input_sizes), len(random_seeds))),
+        'a_star': np.zeros((len(input_sizes), len(random_seeds))),
+        'value_iteration': np.zeros((len(input_sizes), len(random_seeds))),
+        'policy_iteration': np.zeros((len(input_sizes), len(random_seeds)))
+    }
 
-    # for size in input_sizes:
-    #     maze_generator = MazeGenerator(dimension=size, random_seed=42)
-    #     maze = maze_generator.generate_maze()
-    #     maze_solver = MazeSolver(maze)
-    #     bfs_times.append(calculate_execution_time(maze_solver.bfs))
-    #     dfs_times.append(calculate_execution_time(maze_solver.dfs))
-    #     a_star_times.append(calculate_execution_time(maze_solver.a_star))
+    for i, size in enumerate(input_sizes):
+        for j, seed in enumerate(random_seeds):
+            print(f"Creating maze with dimension = {size} and random state = {seed}")
+            maze_generator = MazeGenerator(dimension=size, random_seed=seed)
+            maze = maze_generator.prim()
+            maze_solver = MazeSolver(maze)
+            exec_times['bfs'][i, j] = calculate_execution_time(maze_solver.bfs)
+            exec_times['dfs'][i, j] = calculate_execution_time(maze_solver.dfs)
+            exec_times['a_star'][i, j] = calculate_execution_time(maze_solver.a_star)
+            exec_times['value_iteration'][i, j] = calculate_execution_time(maze_solver.makrov_value_iteration)
+            exec_times['policy_iteration'][i, j] = calculate_execution_time(maze_solver.makrov_policy_iteration)
+
+        for alg, arr in exec_times.items():
+            np.savetxt(f'./results/{alg}.csv', arr, delimiter=',')
 
     # bar_width = 2
     # fig, ax = plt.subplots(figsize=(10, 6))

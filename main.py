5cba89e1d81467e39999fc9ef0cac4e29ee0fe40
George Kanellopoulos <gkanel97@hotmail.com>
2024-02-28 20:00:36 +0000

    Refactored experiment functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+import os
 from maze_solver import MazeSolver
 from maze_generator import MazeGenerator
 from maze_visualiser import MazeVisualiser
@@ -12,24 +13,11 @@ def calculate_execution_time(solver_fn):
     t_end = time.perf_counter()
     return t_end - t_start
 
-if __name__ == '__main__':
-    
-    maze_generator = MazeGenerator(dimension=5, random_seed=17)
-    maze = maze_generator.prim()
-    visualiser = MazeVisualiser(maze)
-    maze_solver = MazeSolver(maze)
-    path, exploration = maze_solver.bfs()
-    visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/bfs.png')
-    path, exploration = maze_solver.dfs()
-    visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/dfs.png')
-    path, exploration = maze_solver.a_star()
-    visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/a_star.png')
-    value_function, path, history = maze_solver.makrov_value_iteration()
-    visualiser.draw_value_policy(history, filename='./figures/value_iteration.gif')
-    policy, value_function, path, history = maze_solver.makrov_policy_iteration()
-    visualiser.draw_value_policy(history, path, filename='./figures/policy_iteration.gif')
+def record_memory_usage(solver_fn):
+    ret_val = solver_fn()
+    return ret_val[-1] # Memory usage is the last element in the return value
 
-
+def execution_time_experiment():
     # Compare the three algorithms for different input sizes
     input_sizes = np.arange(10, 101, 10)
     random_seeds = np.arange(1, 20)
@@ -54,15 +42,59 @@ if __name__ == '__main__':
             exec_times['policy_iteration'][i, j] = calculate_execution_time(maze_solver.makrov_policy_iteration)
 
         for alg, arr in exec_times.items():
-            np.savetxt(f'./results/{alg}.csv', arr, delimiter=',')
+            os.makedirs('./results/exec_time', exist_ok=True)
+            np.savetxt(f'./results/exec_time/{alg}.csv', arr, delimiter=',')
 
-    # bar_width = 2
-    # fig, ax = plt.subplots(figsize=(10, 6))
-    # ax.bar(np.array(input_sizes) - bar_width, bfs_times, width=bar_width, label='BFS')
-    # ax.bar(input_sizes, dfs_times, width=bar_width, label='DFS')
-    # ax.bar(np.array(input_sizes) + bar_width, a_star_times, width=bar_width, label='A*')
-    # ax.set_xlabel('Input size')
-    # ax.set_ylabel('Time (s)')
-    # ax.set_title('Comparison of search algorithms')
-    # ax.legend()
-    # plt.show()
+def memory_usage_experiment():
+    # Compare the three algorithms for different input sizes
+    input_sizes = np.arange(10, 101, 10)
+    random_seeds = np.arange(1, 20)
+    memory_usage = {
+        'bfs': np.zeros((len(input_sizes), len(random_seeds))),
+        'dfs': np.zeros((len(input_sizes), len(random_seeds))),
+        'a_star': np.zeros((len(input_sizes), len(random_seeds))),
+        'value_iteration': np.zeros((len(input_sizes), len(random_seeds))),
+        'policy_iteration': np.zeros((len(input_sizes), len(random_seeds)))
+    }
+
+    for i, size in enumerate(input_sizes):
+        for j, seed in enumerate(random_seeds):
+            print(f"Creating maze with dimension = {size} and random state = {seed}")
+            maze_generator = MazeGenerator(dimension=size, random_seed=seed)
+            maze = maze_generator.prim()
+            maze_solver = MazeSolver(maze)
+            memory_usage['bfs'][i, j] = record_memory_usage(maze_solver.bfs)
+            memory_usage['dfs'][i, j] = record_memory_usage(maze_solver.dfs)
+            memory_usage['a_star'][i, j] = record_memory_usage(maze_solver.a_star)
+            memory_usage['value_iteration'][i, j] = record_memory_usage(maze_solver.makrov_value_iteration)
+            memory_usage['policy_iteration'][i, j] = record_memory_usage(maze_solver.makrov_policy_iteration)
+
+        for alg, arr in memory_usage.items():
+            os.makedirs('./results/memory_usage', exist_ok=True)
+            np.savetxt(f'./results/memory_usage/{alg}.csv', arr, delimiter=',')
+
+def visualise_small_maze():
+    maze_generator = MazeGenerator(dimension=5, random_seed=17)
+    maze = maze_generator.prim()
+    visualiser = MazeVisualiser(maze)
+    maze_solver = MazeSolver(maze)
+    path, exploration = maze_solver.bfs()
+    visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/bfs.png')
+    path, exploration = maze_solver.dfs()
+    visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/dfs.png')
+    path, exploration = maze_solver.a_star()
+    visualiser.draw_search_algorithm(path=path, explored=exploration, filename='./figures/a_star.png')
+    value_function, path, history = maze_solver.makrov_value_iteration()
+    visualiser.draw_value_policy(history, filename='./figures/value_iteration.gif')
+    policy, value_function, path, history = maze_solver.makrov_policy_iteration()
+    visualiser.draw_value_policy(history, path, filename='./figures/policy_iteration.gif')
+            
+if __name__ == '__main__':
+    
+    maze_generator = MazeGenerator(dimension=5, random_seed=5)
+    maze = maze_generator.prim()
+    maze_solver = MazeSolver(maze)
+    value, path, _, _, _ = maze_solver.makrov_policy_iteration()
+    print(path[:20])
+    maze_visualiser = MazeVisualiser(maze)
+    maze_visualiser.draw_search_algorithm(path=path, filename='./figures/problematic.png')
